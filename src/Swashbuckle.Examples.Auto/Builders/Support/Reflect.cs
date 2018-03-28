ff806808e6371043e6c0ea5e57eca57a4a9613cc
Daniel González García <danielgonnet@gmail.com>
2018-03-28 21:25:02 +0200

    support reflection class for built-in builders

---
+++ src/Swashbuckle.Examples.Auto/Builders/Support/Reflect.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Reflection;
+
+namespace Swashbuckle.Examples.Auto.Builders.Support
+{
+	internal static class Reflect
+	{
+		public static bool IsEnum(Type t)
+		{
+			bool isEnum = t.GetTypeInfo().IsEnum;
+			return isEnum;
+		}
+
+		public static bool IsList(Type t)
+		{
+			TypeInfo info = t.GetTypeInfo();
+			bool isList = info.IsGenericType && !info.IsGenericTypeDefinition && typeof(IList).IsAssignableFrom(t);
+			return isList;
+		}
+
+		public static bool IsNullable(Type t)
+		{
+			TypeInfo info = t.GetTypeInfo();
+			bool isNullable = info.IsGenericType &&
+			                  !info.IsGenericTypeDefinition &&
+			                  info.GetGenericTypeDefinition() == typeof(Nullable<>);
+			return isNullable;
+		}
+	}
+}
---
+++ tests/Swashbuckle.Examples.Auto.Tests/Builders/Support/ReflectTester.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using Swashbuckle.Examples.Auto.Builders.Support;
+
+namespace Swashbuckle.Examples.Auto.Tests.Builders.Support
+{
+	[TestFixture]
+	public class ReflectTester
+	{
+		[Test]
+		public void IsEnum_Enum_True()
+		{
+			Assert.That(Reflect.IsEnum(typeof(System.AttributeTargets)), Is.True);
+		}
+
+		[Test]
+		public void IsEnum_NotEnum_False()
+		{
+			Assert.That(Reflect.IsEnum(typeof(System.Action)), Is.False);
+		}
+
+		[Test]
+		public void IsList_OpenGenericList_False()
+		{
+			Assert.That(Reflect.IsList(typeof(System.Collections.Generic.List<>)), Is.False);
+		}
+
+		[Test]
+		public void IsList_ClosedGenericList_True()
+		{
+			Assert.That(Reflect.IsList(typeof(System.Collections.Generic.List<int>)), Is.True);
+		}
+
+		[Test]
+		public void IsList_Array_False()
+		{
+			Assert.That(Reflect.IsList(typeof(int[])), Is.False);
+			Assert.That(Reflect.IsList(typeof(System.Array)), Is.False);
+		}
+
+		[Test]
+		public void IsNullable_OpenNullable_False()
+		{
+			Assert.That(Reflect.IsNullable(typeof(System.Nullable<>)), Is.False);
+		}
+
+		[Test]
+		public void IsNullable_ClosedNullable_True()
+		{
+			Assert.That(Reflect.IsNullable(typeof(int?)), Is.True);
+		}
+	}
+}
