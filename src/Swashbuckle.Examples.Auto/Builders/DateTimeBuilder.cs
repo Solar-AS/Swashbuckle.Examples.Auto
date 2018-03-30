4ab22ced97a66e37f1f8a007f75c895cf22573eb
Daniel González García <danielgonnet@gmail.com>
2018-03-30 14:12:51 +0200

    add DateTimeBuilder

---
+++ src/Swashbuckle.Examples.Auto/Builders/DateTimeBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+
+namespace Swashbuckle.Examples.Auto.Builders
+{
+	public class DateTimeBuilder : SampleBuilderBase
+	{
+		protected override bool CanHandle(CustomAttributeData attribute, PropertyInfo property)
+		{
+			bool canHandle = property.PropertyType == typeof(DateTime);
+			return canHandle;
+		}
+
+		protected override object GetSampleValue(CustomAttributeData attribute, PropertyInfo property)
+		{
+			string sortableDate = (string)attribute.ConstructorArguments.First().Value;
+			DateTime value = DateTime.ParseExact(sortableDate, "s", CultureInfo.InvariantCulture);
+			return value;
+		}
+	}
+}
