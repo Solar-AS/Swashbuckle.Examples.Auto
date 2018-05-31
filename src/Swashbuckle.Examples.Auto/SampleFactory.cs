f4caff86b293c17db0ac2565615875bb6a41b996
Daniel González García <danielgonnet@gmail.com>
2018-05-31 10:46:15 +0200

    document factory

--- src/Swashbuckle.Examples.Auto/SampleFactory.cs
+++ src/Swashbuckle.Examples.Auto/SampleFactory.cs
@@ -5,9 +5,18 @@ using AutoFixture.Kernel;
 
 namespace Swashbuckle.Examples.Auto
 {
+	/// <summary>
+	/// Provides object creation services for classes decorated with the <see cref="SamplifyAttribute"/> attribute.
+	/// </summary>
 	public class SampleFactory
 	{
 		private readonly IFixture _fixture;
+
+		/// <summary>
+		/// Creates an instance of the factory using the provided configuration.
+		/// </summary>
+		/// <param name="config">Configuration action for the <see cref="IFixture"/> instance used to create the instance.
+		/// <para>If <c>null</c>, default configuration using out-of-the-box builders in the <see cref="Swashbuckle.Examples.Auto.Builders"/> namespace.</para></param>
 		public SampleFactory(Action<IFixture> config = null)
 		{
 			_fixture = new Fixture();
@@ -16,6 +25,12 @@ namespace Swashbuckle.Examples.Auto
 			effectiveConfiguration(_fixture);
 		}
 
+		/// <summary>
+		/// Default configuration for the provided <see cref="IFixture"/>.
+		/// </summary>
+		/// <remarks>Uses <see cref="ISpecimenBuilder"/> from the <see cref="Swashbuckle.Examples.Auto.Builders"/> namespace and sets the number
+		/// of elements in collection to 1.</remarks>
+		/// <param name="fixture">Instance to be customized with out-of-the-box <see cref="ISpecimenBuilder"/>.</param>
 		public static void DefaultConfiguration(IFixture fixture)
 		{
 			fixture.RepeatCount = 1;
@@ -29,6 +44,22 @@ namespace Swashbuckle.Examples.Auto
 			fixture.Customizations.Add(new Builders.ScalarBuilder());
 		}
 
+		/// <summary>
+		/// Creates a new specimen of the provided type.
+		/// </summary>
+		/// <remarks>
+		/// An instance will be created if decorated with the <see cref="SamplifyAttribute"/> attribute. Otherwise, <c>null</c> will be returned.
+		/// <para>
+		/// The values for the properties will be:
+		/// <list type="bullet">
+		/// <item><description>A sample value if the property is decorated with the <see cref="SampleAttribute"/>
+		/// and can be built by one of the configured <see cref="ISpecimenBuilder"/> of the <see cref="IFixture"/>.</description></item>
+		/// <item><description>A random value according to the configured <see cref="ISpecimenBuilder"/> of the <see cref="IFixture"/>.</description></item>
+		/// </list>
+		/// </para>
+		/// </remarks>
+		/// <param name="type">The type that specifies what to create.</param>
+		/// <returns>An instace of the provided type with its properties set to samples if decorated, <c>null</c> otherwise.</returns>
 		public object BuildSample(Type type)
 		{
 			object sample = null;
